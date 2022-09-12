544f51f23ecf3da553eeb9747b871ec9aa59f0f0
TharushiJayawardena <tharushijayawardena03@gmail.com>
2022-09-12 16:07:31 +0530

    login

---
+++ LoginPage/loginUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class loginUI : ContentPage
+    {
+        public loginUI()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsername.Text=="admin" && txtPassword.Text == "123")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                DisplayAlert("Ops...", "Username or password  is incorect!", "ok");
+
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
